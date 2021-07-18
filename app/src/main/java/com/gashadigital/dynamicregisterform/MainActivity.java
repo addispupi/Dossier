862d70f56387666cbb3c6967c69aa8c80e42e6f0
Addisu <pupiatpixar@gmail.com>
2021-07-18 22:36:07 +0300

    Picture Uploader - Added

--- app/src/main/java/com/gashadigital/dynamicregisterform/MainActivity.java
+++ app/src/main/java/com/gashadigital/dynamicregisterform/MainActivity.java
@@ -6,18 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSubmit;
+    ImageButton btnPickImg;
+    ImageView imgView;
+    Uri userUri;
+    Bitmap bitmap;
+    public static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSubmit = findViewById(R.id.btn_submit);
+        imgView = findViewById(R.id.img_profile);
+        btnPickImg = findViewById(R.id.btn_upload);
+
+        btnPickImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +52,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, FormResults.class));
             }
         });
+    }
+
+    private void openGallery() {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Profile Picture");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE){
+            userUri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), userUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            imgView.setImageURI(userUri);
+        }
     }
 
     public void setLanguage(Context context, String lan) {
